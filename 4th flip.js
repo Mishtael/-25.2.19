56625cf8d56631fd1338d9e75fca75526cb8a4c2
Mishtael <37247168+Mishtael@users.noreply.github.com>
2019-02-25 19:26:28 +0200

    Update 4th flip.js

--- 4th flip.js
+++ 4th flip.js
@@ -3,6 +3,12 @@ function flip(x)
   x = x*(-1)
   return x
 }
+
 var y = parseInt(prompt("please enter number", 0))
+while (y != 0)
+  {
 flip(y)
 alert(flip(y))
+y = parseInt(prompt("please enter number", 0))
+  }
+alert("0 can't be flipped")
