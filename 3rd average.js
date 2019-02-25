610e3e25f7db3fcc8f99ae03aa4fdebec570cf1e
Mishtael <37247168+Mishtael@users.noreply.github.com>
2019-02-25 19:06:32 +0200

    Create 3rd average.js

---
+++ 3rd average.js
@@ -0,0 +1,9 @@
+function average(a,b,c)
+{
+average = (a+b+c)/3
+alert(average)
+}
+var x = parseInt(prompt("enter first number", 0))
+var y = parseInt(prompt("enter second number", 0))
+var z = parseInt(prompt("enter third number", 0))
+average(x,y,z)
